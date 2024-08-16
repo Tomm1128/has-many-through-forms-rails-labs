72206082acce045199e576d00d0efef3c62a37de
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-16 09:22:26 -0400

    Update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  private
+
+  def all_blank(attributes)
+    attributes['username'].blank?
+  end
 end
